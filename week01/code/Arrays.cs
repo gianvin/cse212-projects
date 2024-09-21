d471b1a517367ca9ce8dce3682398cca024377ec
gianvin <ginamangahassanchezong@gmail.com>
2024-09-21 20:50:32 +0800

    Did Part 1 Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client.Payloads;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,10 +14,24 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        // create an array of size 'length' to store the multiples
+        double[] multiples = new double[length];
+        //Iterate to generate multiples
+        for (int i = 0; i <= length; i++)
+        {
+            //Compute the multiple and add it to the list
+            multiples[i] = number * (i + 1);
+        }
+        // return the result
+        return multiples; // replace this return statement with your own
     }
-
+    // call the static method with number = 3 and length = 5
+    public static void Main(string[] args)
+    {
+        double[] result = MultiplesOf(3, 5);
+        // Output: 3. 6. 9. 12. 15
+        Console.WriteLine(string.Join(", ", result));
+    }
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
     /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
