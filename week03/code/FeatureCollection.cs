221fb76eb459ed91359e1ed2055b894a283c23b8
gianvin <ginamangahassanchezong@gmail.com>
2024-10-05 17:19:41 +0800

    Write the class for Feature, properties and Main class

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,74 @@
+using System.ComponentModel;
+using System.Net.Http.Json;
+using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Extensions;
+
 public class FeatureCollection
 {
     // TODO Problem 5 - ADD YOUR CODE HERE
     // Create additional classes as necessary
+    private const string BaseUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+    public async Task<string> EarthquakeDailySummary()
+    {
+        using HttpClient httpClient = new HttpClient();
+
+        try
+        {
+            // Fetch the JSON DATA from USGS API
+            var response = await httpClient.GetAsync(BaseUrl);
+            response.EnsureSuccessStatusCode();
+
+            //Deserialize the JSON response
+
+            var earthquakeData = await response.Content.ReadFromJsonAsync<EarthquakeData>();
+
+            // Earthquake locations and magnitude
+            List<string> formattedStrings = new List<string>();
+            foreach (var feature in earthquakeData.Features)
+            {
+                // Format the output string using 'place' and  'mag'
+                string formattedString = $"{feature.Properties.Place} - Mag {feature.Properties.Mag}";
+                formattedString.Add(formattedString);
+            }
+
+            // join all the formatted string into a single output
+            return $"[{string.Join(", ", formattedStrings)}]";
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching eartquake data: {The request timed out. Please try again later}");
+            return "[]";
+        }
+    }
+
+    private int EarthquakeData()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class EarthquakeData
+{
+    public List<FeatureCollection> Features { get; set; }
+
+}
+
+public class Features
+{
+    public Properties Properties { get; set; }
+}
+
+public class Properties
+{
+    public string Place { get; set; } // 'place' attribute for location
+    public double Mag { get; set; } // 'mag' attribute for magnitude
+}
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        FeatureCollection featureCollection = new FeatureCollection();
+        string earthquakeSummary = await featureCollection.EarthquakeDailySummary();
+        Console.WriteLine(earthquakeSummary);
+    }
 }
