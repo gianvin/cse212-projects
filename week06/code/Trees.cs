db09e53c625128d8e3064322bf9456ca75b9ace7
gianvin <ginamangahassanchezong@gmail.com>
2024-10-23 13:44:05 +0800

    write the code for problem no. 5

--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -49,5 +49,15 @@ public static class Trees
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
         // TODO Start Problem 5
+        if (first > last)
+        {
+            return; // base case
+        }
+
+        int middle = (first + last) / 2;// find the middle index
+        bst.Insert(sortedNumbers[middle]); // insert the middle element into the bst
+
+        InsertMiddle(sortedNumbers, first, middle - 1, bst);
+        InsertMiddle(sortedNumbers, middle + 1, last, bst);
     }
 }
